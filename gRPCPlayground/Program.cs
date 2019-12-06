87591307d58eb10e2a9f55332e8c532fcab67e6c
juanpablot <juanpablot@lagash.com>
2019-12-05 21:26:28 -0300

    initial commit included Grpc.Tools package to Services project
     On branch master
    	new file:   .gitignore
    	new file:   Services/Class1.cs
    	new file:   Services/Services.csproj
    	new file:   gRPCPlayground.sln
    	new file:   gRPCPlayground/Program.cs
    	new file:   gRPCPlayground/gRPCPlayground.csproj

---
+++ gRPCPlayground/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace gRPCPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var j = new juan {  edad=15,nombre="gri"};
+             (var edad,var nombre  ) = j;
+
+
+
+
+
+            Console.WriteLine($"Hello World! {edad},  {nombre}  ");
+        }
+    
+    }
+
+
+    class juan
+    {
+
+        public int edad { get; set; }
+        public string nombre { get; set; }
+
+        internal void Deconstruct(out int edad, out string nombre)
+        {
+            edad = this.edad;
+            nombre = this.nombre;
+        }
+    }
+
+}
