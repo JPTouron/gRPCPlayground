88ec644f80b2ec1c834f815c4a965a90132808f0
juanpablot <juanpablot@lagash.com>
2019-12-05 22:53:35 -0300

    Created new service PerformerService
     On branch master
    	modified:   GrpcService1/Protos/greet.proto
    	new file:   GrpcService1/Services/PerformerService.cs
    	modified:   GrpcService1/Startup.cs
    	modified:   gRPCPlayground/Program.cs
    	modified:   gRPCPlayground/Protos/greet.proto

---
+++ GrpcService1/Services/PerformerService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcService1.Services
+{
+    public class PerformerService : Performer.PerformerBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        public PerformerService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<PerformReply> PerformIt(PerformRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new PerformReply
+            {
+                Age = 19,
+                Name = request.Name
+            });
+        }
+    }
+}
--- gRPCPlayground/Program.cs
+++ gRPCPlayground/Program.cs
@@ -7,17 +7,33 @@ namespace gRPCPlayground
 {
     internal class Program
     {
-        private static async Task Main(string[] args)
+        private static async Task GreeterServiceInvoke(GrpcChannel channel)
         {
-
-            // The port number(5001) must match the port of the gRPC server.
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
+        }
+
+        private static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            await GreeterServiceInvoke(channel);
+
+            await PerformerServiceInvoke(channel);
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static async Task PerformerServiceInvoke(GrpcChannel channel)
+        {
+            var clientPerformer = new Performer.PerformerClient(channel);
+            var perfReply = await clientPerformer.PerformItAsync(
+                                new PerformRequest { Name = "Harry" });
+
+            Console.WriteLine($"Performer chosen: Name:'{perfReply.Name}', Age: '{perfReply.Age}'");
         }
     }
 }
