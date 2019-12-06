81c02a710d662dc458f143684bf8a2a07ec2cc2e
juanpablot <juanpablot@lagash.com>
2019-12-05 21:27:10 -0300

    cleared up program.cs file

--- gRPCPlayground/Program.cs
+++ gRPCPlayground/Program.cs
@@ -2,36 +2,11 @@
 
 namespace gRPCPlayground
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
-
-            var j = new juan {  edad=15,nombre="gri"};
-             (var edad,var nombre  ) = j;
-
-
-
-
-
-            Console.WriteLine($"Hello World! {edad},  {nombre}  ");
-        }
-    
-    }
-
-
-    class juan
-    {
-
-        public int edad { get; set; }
-        public string nombre { get; set; }
-
-        internal void Deconstruct(out int edad, out string nombre)
-        {
-            edad = this.edad;
-            nombre = this.nombre;
+            Console.WriteLine($"Hello World!  ");
         }
     }
-
 }
